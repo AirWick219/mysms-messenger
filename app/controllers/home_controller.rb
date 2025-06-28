dfdc0a734abe217a08ad75b4bd38b07dc507eb6a
Eric Fung <Eric.Fung@cerner.com>
2025-06-28 00:34:18 -0600

    Force to use env

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   skip_before_action :require_authentication
   def index
+    puts "[HOME_CONTROLLER] Serving index.html"
+
     render file: Rails.root.join('public', 'index.html'), layout: false
   end
 end
